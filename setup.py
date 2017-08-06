f0111c9a965c9a9ecf87309d027bf9ef429e216f
tasi788 <tasi788@gmail.com>
2017-08-06 13:28:20 +0800

    upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2008-2013 Erik Svensson <erik.public@gmail.com>
+# Licensed under the MIT license.
+
+from setuptools import setup
+
+required = ['six>=1.1.0']
+
+setup(
+    name='transmissionrpc',
+    version='0.11',
+    description='Python module that implements the Transmission bittorent client RPC protocol.',
+    author='Erik Svensson',
+    author_email='erik.public@gmail.com',
+    url='http://bitbucket.org/blueluna/transmissionrpc',
+    keywords='transmission bittorent torrent',
+    packages=['transmissionrpc'],
+    install_requires = required,
+    test_suite = "test",
+    zip_safe=True,
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Internet'
+        ],
+    )
